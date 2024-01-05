32f927b52124eb6164dc079a47586db2848395c2
Oleg Asimov <arounte@proton.me>
2024-01-06 00:15:59 +0700

    add nextui

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,13 @@
+import { Chip } from '@nextui-org/react'
+
 function App(): JSX.Element {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-extrabold text-blue-500">Zenith Boilerplate</h1>
+      <h1 className="text-3xl font-extrabold text-blue-500">Zenith Boilerplate</h1>
       <a href="https://github.com/arounte">
-        <h2 className="text-lg font-light text-blue-400">GitHub: @Arounte</h2>
+        <h2 className="text-lg font-light text-blue-400 hover:text-pink-500">GitHub: @Arounte</h2>
       </a>
+      <Chip color="primary" variant="shadow" className="my-2">NextUI works 🍻</Chip>
     </div>
   )
 }
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/index.css'
 import App from './App'
+import { NextUIProvider } from '@nextui-org/react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>
 )
